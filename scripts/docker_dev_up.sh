187ab3e1fb4ea78229570af5952e109c282a331a
kmj36 <kmj36953695@gmail.com>
2026-06-25 20:38:41 +0900

    [FIX] Update docker-compose commands to include detached mode for dev, prod, and staging scripts

--- scripts/docker_dev_up.sh
+++ scripts/docker_dev_up.sh
@@ -3,4 +3,4 @@ sudo docker-compose \
   -f ../infra/compose/docker-compose.base.yml \
   -f ../infra/compose/docker-compose.dev.yml \
   --env-file ../infra/env/.env.dev \
-  up --build
+  up -d --build
