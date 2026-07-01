13a5eb8d503d9a115f059265864794e52a5a2086
kmj36 <kmj36953695@gmail.com>
2026-07-01 15:52:21 +0900

    [CHORE] Update deployment configurations and scripts for development and staging environments

--- scripts/docker_staging_up.sh
+++ scripts/docker_staging_up.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 cd "$SCRIPT_DIR/.." || exit 1
 
-sudo docker-compose \
+docker-compose \
   -f infra/compose/docker-compose.base.yml \
   -f infra/compose/docker-compose.staging.yml \
   --env-file infra/env/.env.staging \
