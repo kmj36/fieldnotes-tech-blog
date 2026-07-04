67c0e884a969b3f2a6159096e0b18cc10ef59539
kmj36 <kmj36953695@gmail.com>
2026-07-04 15:34:00 +0900

    [UPDATE] Add project name flag to docker-compose down scripts for dev, prod, and staging environments

--- scripts/docker_prod_down.sh
+++ scripts/docker_prod_down.sh
@@ -1,5 +1,6 @@
 # Prod
 docker-compose \
+  -p fieldnotes-production \
   -f ../infra/compose/docker-compose.base.yml \
   -f ../infra/compose/docker-compose.prod.yml \
   --env-file ../infra/env/.env.prod \
